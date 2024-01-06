b13284b08eb6e6bb1f89ab5dd56623f0b7bacf79
Noxlight7 <noxlight7@mail.ru>
2024-01-06 23:19:37 +0300

    Add connection between servers

---
+++ client/src/Pages/Data.js
@@ -0,0 +1,214 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Helmet } from 'react-helmet';
+import React, {useState} from 'react';
+import axios from 'axios'
+
+const SERVER = 'http://localhost:5000'
+const DATA_PATH = '/data'
+
+const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+
+const $host = axios.create({
+    baseURL: SERVER
+})
+
+const PostItem = (props) => {
+    const [post, setPost] = useState(props.getPost(props.index))
+    return (
+        <div>
+            <row>
+                <input
+                    value={post.title}
+                    onChange={e => {
+                        props.setPost(props.index, {...post, title: e.target.value})
+                        setPost({...post, title: e.target.value})
+                    }}
+                    type="text"
+                    placeholder="Название поля"
+                />
+                <input
+                    value={post.value}
+                    onChange={e => {
+                        props.setPost(props.index, {...post, value: e.target.value})
+                        setPost({...post, value: e.target.value})
+                    }}
+                    type="text"
+                    placeholder="Значение поля"
+                />
+                <button onClick={() => props.remove(props.getPost(props.index).id)}>Удалить</button>
+            </row>
+        </div>
+    );
+};
+
+const LongPostItem = (props) => {
+    const [post, setPost] = useState(
+        {title: props.post.title, value: props.post.value})
+    return (
+        <div>
+            <row>
+                <input
+                    value={post.title}
+                    onChange={e => {
+                        setPost({...post})
+                    }}
+                    type="text"
+                    placeholder="Название поля"
+                />
+                <input
+                    value={post.value}
+                    onChange={e => {
+                        Object.assign(props.post, {...post, value: e.target.value})
+                        setPost({...post, value: e.target.value})
+
+                    }}
+                    type="text"
+                    placeholder="Значение поля"
+                />
+            </row>
+        </div>
+    );
+};
+
+const PostList = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            {
+                props.postCountArray.map((x, i) =>
+                    <PostItem
+                        remove={props.remove}
+                        getPost={props.getPost}
+                        setPost={props.setPost}
+                        index={i}
+                        key={props.getPost(i).id}/>
+            )}
+        </div>
+    );
+};
+
+const PostForm = (props) => {
+    const addNewPost = (e) => {
+        e.preventDefault()
+        props.create()
+    }
+
+    const getData = (e) => {
+        e.preventDefault()
+        props.get()
+    }
+
+    const postData = (e) => {
+        e.preventDefault()
+        props.post()
+    }
+
+    return (
+        <form>
+            <button onClick={addNewPost}>Добавить поле</button>
+            <button onClick={postData}>Добавить данные на сервер</button>
+            <button onClick={getData}>Получить данные с сервера</button>
+        </form>
+    );
+};
+
+const posts = [
+    {id:3, title: 'title', value: 'ИТ-211'},
+]
+
+let sectionPost = {id: 1, title: 'section', value: 'groups'}
+let IDPost = {id: 2, title: 'id', value: '1'}
+
+function DataPage() {
+
+    const setPost = (index, value) => {
+        Object.assign(posts[index], value)
+    }
+
+    const getPost = (index) => {
+        const retPost = {id: 0, title: '', value: ''}
+        Object.assign(retPost, posts[index])
+        return retPost
+    }
+
+    const [postCountArray, setPostCountArray] = useState([1])
+
+    const createPost = async () => {
+        const newPost = {title: '', value: '', id: Date.now()}
+        posts.push(newPost)
+        await setPostCountArray([...postCountArray, 1])
+    }
+
+    const removePost = (post) => {
+        //posts = posts.filter(p => p.id !== post.id)
+    }
+
+    const postData = async () => {
+        const sendData = {
+            id: IDPost.value,
+            section: sectionPost.value,
+        }
+
+        posts.map(item => {
+            sendData[item.title] = item.value
+        })
+
+        console.log('Отправка данных', sendData);
+
+        try {
+            const response = await $host.post(
+                '/data/post',
+                sendData, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                })
+            console.log('Ответ сервера', response.data);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    const getData = async () => {
+        const searchData = {
+            id: IDPost.value,
+            section: sectionPost.value,
+        }
+        console.log('Поиск данных по', searchData);
+
+        try {
+            const response = await $host.post(
+                '/data/get',
+                searchData, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+
+                })
+            console.log('Ответ сервера', response.data);
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    return (
+        <div>
+            <Helmet>
+                <title>Данные</title>
+            </Helmet>
+
+            <LongPostItem post={sectionPost}/>
+            <LongPostItem post={IDPost}/>
+            <PostForm create={createPost} get={getData} post={postData}/>
+            <PostList
+                remove={removePost}
+                setPost={setPost}
+                getPost={getPost}
+                postCountArray={postCountArray}
+            />
+        </div>
+    );
+}
+
+export default DataPage;
--- server/routes.py
+++ server/routes.py
@@ -1,9 +1,96 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, json, jsonify, make_response
+from flask_cors import CORS, cross_origin
 SECRET_KEY = 'v7}efa{9I$FjOB9*D&6iiD52ugi;o:W'
 
 app = Flask(__name__)
+CORS(app,
+     resources={r"/data/*": {"origins": "http://localhost:3000"}})
 app.config.from_object(__name__)
+
+# Хранит данные следующим образом:
+# Ключами выступают категории(строка) данных
+# Значение - это словарь, ключи которого
+# целые ID
+data = {
+    'groups' : {
+        1: {'title': 'ПИ-211'},
+        2: {'title': 'ПИ-212'},
+        3: {'title': 'ПИ-221'},
+        4: {'title': 'ПИ-222'},
+        5: {'title': 'ПИ-231'},
+        6: {'title': 'ПИ-232'},
+        7: {'title': 'ПИ-233'},
+    }
+}
+
+SECTION_TITLE = 'section'
+ID_TITLE = 'id'
+RESULT_TITLE = 'result'
+
+def retRes(result):
+    return jsonify({RESULT_TITLE: result})
+
+@app.route("/data/post", methods=["POST", "OPTIONS"])
+def postData():
+    if request.method == "POST":
+        val = request.json
+        section = val[SECTION_TITLE]
+        del val[SECTION_TITLE]
+        id = int(val[ID_TITLE])
+        del val[ID_TITLE]
+
+        if section not in data.keys():
+            data[section] = {}
+        data[section][id] = val
+
+        return retRes(True)
+
+    return retRes(False)
+
+
+@app.route("/data/get", methods=["POST"])
+def getData():
+    if request.method == "POST":
+        section = request.json.get(SECTION_TITLE)
+        id = int(request.json.get(ID_TITLE))
+
+        if section not in data.keys() or id not in data[section].keys():
+            return retRes(False)
+
+        return jsonify(data[section][id])
+
+    return retRes(False)
+
+
+@app.before_request
+def bfr_user():
+    if request.method == "OPTIONS":
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        response.headers.add('Access-Control-Request-Method', 'GET, POST')
+        response.headers.add('Access-Control-Request-Headers', 'Content-Type')
+        return response
+
+
+
+@app.after_request
+def ar_user(response):
+    if request.method != "OPTIONS" and response.status_code == 200:
+        #if response.
+        if RESULT_TITLE not in response.json.keys():
+            newData = response.get_json()
+            newData.update({RESULT_TITLE: True})
+            response = jsonify(newData)
+
+        response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+@app.teardown_request
+def tr_user(response):
+    return response
 
 
 @app.route('/')
