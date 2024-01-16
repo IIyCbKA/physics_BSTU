b60b044691493e7bb48be884085ca791db8b2d10
IIyCbKA <c54go@yandex.ru>
2024-01-16 21:52:57 +0300

    clean back

--- server/routes.py
+++ server/routes.py
@@ -1,66 +1,11 @@
-from flask import Flask, render_template, request, json, jsonify, make_response
+from flask import request, jsonify
 from application import app
 
-# Хранит данные следующим образом:
-# Ключами выступают категории(строка) данных
-# Значение - это словарь, ключи которого
-# целые ID
-data = {
-    'groups' : {
-        1: {'title': 'ПИ-211'},
-        2: {'title': 'ПИ-212'},
-        3: {'title': 'ПИ-221'},
-        4: {'title': 'ПИ-222'},
-        5: {'title': 'ПИ-231'},
-        6: {'title': 'ПИ-232'},
-        7: {'title': 'ПИ-233'},
-    }
-}
-
-SECTION_TITLE = 'section'
-ID_TITLE = 'id'
 RESULT_TITLE = 'result'
 
 
-def retRes(result):
+def reternResult(result):
     return jsonify({RESULT_TITLE: result})
-
-
-@app.route("/api/data/post", methods=["POST"])
-def postData():
-    if request.method == "POST":
-        val = request.json
-        section = val[SECTION_TITLE]
-        del val[SECTION_TITLE]
-        id = int(val[ID_TITLE])
-        del val[ID_TITLE]
-
-        if section not in data.keys():
-            data[section] = {}
-        data[section][id] = val
-
-        return retRes(True)
-
-    return retRes(False)
-
-
-@app.route("/api/data/get", methods=["POST"])
-def getData():
-    if request.method == "POST":
-        section = request.json.get(SECTION_TITLE)
-        id = int(request.json.get(ID_TITLE))
-
-        if section not in data.keys() or id not in data[section].keys():
-            return retRes(False)
-
-        return jsonify(data[section][id])
-
-    return retRes(False)
-
-
-@app.before_request
-def bfr_user():
-    pass
 
 
 @app.after_request
@@ -72,9 +17,4 @@ def ar_user(response):
             response = jsonify(newData)
 
         response.headers['Content-Type'] = 'application/json'
-    return response
-
-
-@app.teardown_request
-def tr_user(response):
     return response
--- server/users.py
+++ server/users.py
@@ -2,7 +2,7 @@ from flask import request
 from application import app
 
 
-@app.route("/api/login", methods=["POST", "OPTIONS"])
+@app.route("/api/login", methods=["POST"])
 def login():
     req_data = request.get_json()
     _mail = req_data.get('mail')
