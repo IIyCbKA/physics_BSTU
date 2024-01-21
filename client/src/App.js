f60a52ac6339057cd346f5791462e4aef49ac118
IIyCbKA <c54go@yandex.ru>
2024-01-21 13:33:32 +0300

    fix errors & add route to tests

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Login from './Pages/Login';
+import Test from "./Pages/Test";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/tests" element={<Test />} />
           </Routes>
       </Router>
   );
