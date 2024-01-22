38239ff9ff21f9858dfbe52c4699ceb4de91c305
Виталий Курбанов <vitalikhey2017@gmail.com>
2024-01-22 17:07:48 +0300

    Test version store for check

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Login from './Pages/Login';
 import Test from "./Pages/Test";
+import React from 'react';
 
 function App() {
   return (
