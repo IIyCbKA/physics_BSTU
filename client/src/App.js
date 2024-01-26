8e86ffc615ecd87c8626b1bea29e2dd4027f5e6a
IIyCbKA <c54go@yandex.ru>
2024-01-26 12:18:39 +0300

    clean code

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Pages/Home";
 import Login from './Pages/Login';
 import Test from "./Pages/Test";
-import React from 'react';
-import Disk from "./Components/Disk";
 
 function App() {
   return (
@@ -14,7 +12,6 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/tests" element={<Test />} />
-              <Route path="/" element={<Disk />} />
           </Routes>
       </Router>
   );
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,8 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "../Components/Header";
 import {Helmet} from 'react-helmet';
-import Counter from "../feathures/counter/Counter";
-import Disk from "../Components/Disk"
 
 function Home() {
     return (
@@ -11,8 +9,6 @@ function Home() {
                 <title>Физика</title>
             </Helmet>
             <Header/>
-            <Counter/>
-            <Disk/>
         </div>
     );
 }
