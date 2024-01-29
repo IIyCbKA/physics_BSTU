b71660936244e34bbae915d1dc1538e1de8e13a7
Noxlight7 <noxlight7@mail.ru>
2024-01-30 01:59:07 +0300

    hotfix 0000008

--- server/run.py
+++ server/run.py
@@ -6,5 +6,5 @@ import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, port=port, debug=False, allow_unsafe_werkzeug=True)
+    socketio.run(app, port=port, debug=False)
     db.close()
