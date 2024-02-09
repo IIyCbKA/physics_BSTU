cd59bf960d7532f260c3634859692a5062ec0aab
IIyCbKA <c54go@yandex.ru>
2024-02-09 12:40:25 +0300

    add drag-and-drop

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,30 +2,71 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from "../components/header";
 import {Helmet} from 'react-helmet';
 import {uploadFile} from '../actions/files'
-import Filelist from "../components/Filelist";
+import {useState} from "react";
+import '../styles/style.css'
 
 function Home() {
+    const [dragEnter, setDragEnter] = useState(false);
+
     function fileUploadHandler(event) {
         const files = [...event.target.files]
         files.forEach(file => uploadFile(file, '/'))
 
     }
 
+    function dragEnterHandler(event){
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(true)
+    }
+
+    function dragLeaveHandler(event){
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(false)
+    }
+
+    function dropHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        let files = [...event.dataTransfer.files]
+        files.forEach(file => uploadFile(file, '/'))
+        setDragEnter(false)
+    }
+
     return (
-        <div>
-            <Helmet>
-                <title>Физика</title>
-            </Helmet>
-            <Header/>
-            <input
-                onInput={fileUploadHandler}
-                multiple={true}
-                type="file"
-                id="disk_upload_input"
-                className="disk_upload_input">
-            </input>
-            <Filelist/>
-        </div>
+        !dragEnter ?
+            <div
+                onDragEnter={dragEnterHandler}
+                onDragLeave={dragLeaveHandler}
+                onDragOver={dragEnterHandler}
+            >
+                <Helmet>
+                    <title>Физика</title>
+                </Helmet>
+                <Header/>
+                <input
+                    onInput={fileUploadHandler}
+                    multiple={true}
+                    type="file"
+                    id="disk_upload_input"
+                    className="disk_upload_input">
+                </input>
+            </div>
+        :
+            <div>
+                <Header/>
+                <div
+                    className='drop-area'
+                    onDragEnter={dragEnterHandler}
+                    onDragLeave={dragLeaveHandler}
+                    onDragOver={dragEnterHandler}
+                    onDrop={dropHandler}
+                >
+                    Перетащите файлы сюда
+                </div>
+            </div>
+
     );
 }
 
