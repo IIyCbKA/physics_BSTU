72f9ba1e31ae0e410cd3cfbbe98309884e96711a
IIyCbKA <c54go@yandex.ru>
2024-03-03 01:03:06 +0300

    clean

--- server/application.py
+++ server/application.py
@@ -4,17 +4,10 @@ from server.settings.config import CLIENT_URL
 
 fastApiServer = FastAPI()
 
-# Разрешаем CORS для всех доменов (настраивайте по необходимости)
 fastApiServer.add_middleware(
     CORSMiddleware,
-    allow_origins=[CLIENT_URL],  # Разрешаем доступ с любых доменов
+    allow_origins=[CLIENT_URL],
     allow_credentials=True,
     allow_methods=["*"],  # Разрешаем все HTTP методы
     allow_headers=["*"],  # Разрешаем все заголовки
 )
-
-
-@fastApiServer.get('/api/temp/')
-async def filesList():
-
-    return {'asd': 45}, 200
