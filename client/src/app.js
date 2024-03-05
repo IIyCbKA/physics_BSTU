d9346eb66363d3dbb59bd54531877c37f60e105c
IIyCbKA <c54go@yandex.ru>
2024-03-05 22:30:38 +0300

    add Not Found page & add redirect start page to disk

--- client/src/app.js
+++ client/src/app.js
@@ -1,20 +1,25 @@
 import './styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from "./pages/home";
 import Login from './pages/login';
 import Test from "./pages/test";
+import {NotFound} from "./pages/not_found";
+
 
 function App() {
   return (
       <Router>
           <Routes>
+              <Route path="/" element={<Navigate to="/disk/" />} />
               <Route path="/disk*" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/tests" element={<Test />} />
+              <Route path="*" element={<NotFound />} />
           </Routes>
       </Router>
   );
 }
+
 
 export default App;
