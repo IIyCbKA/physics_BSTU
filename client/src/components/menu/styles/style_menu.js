bf3f2530840d57b9661d1e9a1edae434ed89d4e1
IIyCbKA <c54go@yandex.ru>
2024-04-15 02:32:57 +0300

    microfix

--- client/src/components/menu/styles/style_menu.js
+++ client/src/components/menu/styles/style_menu.js
@@ -3,7 +3,7 @@ const customOffcanvas = {
     transitionDuration: '1s',
     backgroundColor: 'rgba(255, 255, 255, 0.75)',
     backdropFilter: 'blur(10px)',
-    webkitBackdropFilter: 'blur(10px)'
+    WebkitBackdropFilter: 'blur(10px)'
 }
 
 export const styles = {
