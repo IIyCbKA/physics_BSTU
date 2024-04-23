82b22ab1094619db8df97d9413cc59b69a73afb8
IIyCbKA <c54go@yandex.ru>
2024-04-23 17:13:43 +0300

    microfix

--- client/src/elements/account/task_form/information/information.js
+++ client/src/elements/account/task_form/information/information.js
@@ -2,7 +2,7 @@ import './styles/styles_form_informations.css'
 import TitleInputLine from "./input_lines/title/title_input_line";
 import DescriptionInputLine
     from "./input_lines/description/description_input_line";
-import AdditionEntity from "../addition/enity/addition_entity";
+import AdditionEntity from "../addition/entity/addition_entity";
 
 
 export default function Information(props){
