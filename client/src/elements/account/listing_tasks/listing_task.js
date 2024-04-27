4835984c55767e52a0a46d0a3d4b70d7d5c68729
IIyCbKA <c54go@yandex.ru>
2024-04-27 22:00:33 +0300

    update listing task

--- client/src/elements/account/listing_tasks/listing_task.js
+++ client/src/elements/account/listing_tasks/listing_task.js
@@ -1,26 +1,18 @@
 import {useSelector} from "react-redux";
 import './styles/style_listing_task.css'
-import React, {useEffect} from "react";
 import Task from "./task/task";
 
 export default function ListingTask(){
     const tasks = useSelector(state => state.journal.tasks);
 
-    useEffect(() => {
-        const taskElements = document.querySelectorAll('.task-main');
-        if (taskElements.length > 0){
-            const lastTask = taskElements[taskElements.length - 1];
-            lastTask.style.borderBottom = 'none';
-        }
-    }, [tasks]);
-
     return (
         <div className='listing-task-root'>
-            {tasks.map(task => (
+            {tasks.map((task, index) => (
                 <Task title={task.title}
                       description={task.description}
                       id={task.id}
                       key={task.id}
+                      isLast={index === tasks.length - 1}
                 />
             ))}
         </div>
--- client/src/elements/account/listing_tasks/task/task.js
+++ client/src/elements/account/listing_tasks/task/task.js
@@ -1,11 +1,56 @@
 import './styles/style_task.css'
 import {BookOutlined} from "@ant-design/icons";
 import {styles} from './styles/style_task'
+import {useState} from "react";
 
 export default function Task(props){
+    const [isActive, setActive] = useState(false);
+
+    const rootStyle = () => {
+        if (isActive){
+            return {boxShadow: '0 1px 2px 0 rgba(60,64,67,.3), ' +
+                        '0 2px 6px 2px rgba(60,64,67,.15)',
+                    borderTopLeftRadius: '0px',
+                    borderTopRightRadius: '0px'
+            }
+        }
+    }
+
+    const mainStyle = () =>{
+        if (isActive){
+            return {borderBottom: 'none',
+                    backgroundColor: '#E8F0FE'
+            }
+        } else if (!props.isLast){
+            return {borderBottom: '1px solid #E0E0E0'}
+        }
+    }
+
+    const infoStyle = () => {
+        const defaultStyle = {
+            transition: 'height .3s cubic-bezier(0.4, 0, 0.2, 1)'
+        }
+
+        if (isActive){
+            return {...defaultStyle,
+                    height: '300px',
+                    borderTop: '1px solid #E0E0E0'
+            }
+        } else{
+            return {...defaultStyle,
+                    height: '0',
+            }
+        }
+    }
+
     return(
-        <div className='task-root'>
-            <div className='task-main'>
+        <div className='task-root'
+             style={rootStyle()}
+        >
+            <div className='task-main'
+                 style={mainStyle()}
+                 onClick={() => setActive(!isActive)}
+            >
                 <div className='task-icon-wrap'>
                     <BookOutlined style={styles.iconStyle}/>
                 </div>
@@ -14,6 +59,9 @@ export default function Task(props){
                         {props.title}
                     </div>
                 </div>
+            </div>
+            <div className='task-info-wrap' style={infoStyle()}>
+
             </div>
         </div>
     )
