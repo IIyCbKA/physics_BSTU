e3046ebc1a9bbb5975bf50e5b6955819c39086de
IIyCbKA <c54go@yandex.ru>
2024-05-02 17:02:06 +0300

    bugfix

--- client/src/elements/account/navigate_button/styles/style_navigate_btn.js
+++ client/src/elements/account/navigate_button/styles/style_navigate_btn.js
@@ -1,36 +1,42 @@
 const btnCommonStyle = {
-    border: 'none',
     borderRadius: 0,
     padding: '0 20px',
     backgroundColor: '#FFFFFF',
-    borderBottom: '3px solid #1967D2',
-    borderBottomRightRadius: '3px',
-    borderBottomLeftRadius: '3px'
+    borderBottomRightRadius: '4px',
+    borderBottomLeftRadius: '4px'
 };
 
 const btnDefaultStyle = {
     ...btnCommonStyle,
     backgroundColor: '#FFFFFF',
     color: '#5F6368',
-    borderBottom: 'None'
+    border: '3px solid #FFFFFF',
 };
 
 const btnFocusStyle = {
     ...btnCommonStyle,
     backgroundColor: '#F7F7F7',
     color: '#161616',
-    borderBottom: 'None'
+    border: '3px solid #F7F7F7',
 };
 
 const btnSelectedAndFocus = {
     ...btnCommonStyle,
     backgroundColor: '#E8F0FE',
     color: '#1967D2',
+    borderTop: '3px solid #E8F0FE',
+    borderLeft: '3px solid #E8F0FE',
+    borderRight: '3px solid #E8F0FE',
+    borderBottom: '3px solid #1967D2'
 }
 
 const btnSelectedAndNonFocus = {
     ...btnCommonStyle,
     color: '#1967D2',
+    borderTop: '3px solid #FFFFFF',
+    borderLeft: '3px solid #FFFFFF',
+    borderRight: '3px solid #FFFFFF',
+    borderBottom: '3px solid #1967D2'
 }
 
 export const styles = {
