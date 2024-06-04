1e216f0e5d98a6764fc353974d25f7cbe4eff1a9
IIyCbKA <c54go@yandex.ru>
2024-06-04 13:13:29 +0300

    add file info to file header

--- client/src/components/header/file_header/dropdown_file_info.js
+++ client/src/components/header/file_header/dropdown_file_info.js
@@ -2,17 +2,35 @@ import {useSelector} from "react-redux";
 import {styles} from "./styles/style_file_header";
 import {InfoOutlined} from "@mui/icons-material";
 import {Nav} from "react-bootstrap";
+import {Dropdown} from "antd";
+import './styles/style_file_header.css'
+
+const DROPDOWN_NAME_TITLE = 'Имя:'
 
 export default function DropdownFileInfo(){
     const selected_name = useSelector(state => state.file.selected_name)
 
-    const handleInfoClick = () => {
-        // тут переписать
-    };
+    const customDropdown = () => (
+        <div className='dropdown-wrap'>
+            <div className='dropdown-text-line'>
+                <div className='dropdown-text-line-title'>
+                    {DROPDOWN_NAME_TITLE}
+                </div>
+                <div className='dropdown-text-line-main'>
+                    {selected_name}
+                </div>
+            </div>
+        </div>
+    );
 
     return (
-        <Nav.Item style={styles.navItemStyle} onClick={handleInfoClick}>
-            <InfoOutlined style={styles.iconsStyle}/>
-        </Nav.Item>
+        <Dropdown
+            dropdownRender={customDropdown}
+            trigger={['click']}
+        >
+            <Nav.Item style={styles.navItemStyle}>
+                <InfoOutlined style={styles.iconsStyle}/>
+            </Nav.Item>
+        </Dropdown>
     )
 }
