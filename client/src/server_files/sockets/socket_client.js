f9af64a63870f43bf074ac5a7b223ed4ae45bf44
Noxlight7 <noxlight7@mail.ru>
2024-06-14 00:18:36 +0300

    add sockets reconnect

--- client/src/server_files/sockets/socket_client.js
+++ client/src/server_files/sockets/socket_client.js
@@ -1,10 +1,12 @@
 import { SERVER_ADR } from "../server_connect";
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 class SocketManager {
   constructor(address, ws) {
     this.routes = {};
     this.address = address;
     this.ws = ws;
+    this.initSockets = []
   }
 
   onMessage(routeName, routeFunc) {
@@ -12,20 +14,34 @@ class SocketManager {
   }
 
   async init(routeName, data) {
+    this.initSockets.push({routeName, data})
+    this.send(routeName, data)
+  }
+
+  async send(routeName, data) {
     const token = localStorage.getItem("token");
-    this.ws.send(JSON.stringify({ routeName, token, data }));
+    this.ws.send(JSON.stringify({ routeName, token, data}));
+  }
+
+  async reconnect() {
+    this.initSockets.map(socket => this.send(socket.routeName, socket.data))
   }
 }
 
 const address = "ws://" + SERVER_ADR + "/ws";
 
-const wsocket = new WebSocket(address);
+const wsocket = new ReconnectingWebSocket(address);
 
 const socket = new SocketManager(address, wsocket);
 
 wsocket.onmessage = (event) => {
   const data = JSON.parse(event.data);
   socket.routes[data.routeName](data.data);
+};
+
+wsocket.onopen = () => {
+  if (socket)
+    socket.reconnect();
 };
 
 // Реализовать позже переподключение при смене сети
