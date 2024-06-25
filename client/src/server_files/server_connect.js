c41bcc93812b775db6edbb3e1c481a900fb30f28
noxlight <noxlight7@mail.ru>
2024-06-26 02:53:45 +0300

    fix docker3

--- client/src/server_files/server_connect.js
+++ client/src/server_files/server_connect.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const SERVER_ADR = 'server:5000'
+export const SERVER_ADR = 'nginx/api'
 export const SERVER = 'http://' + SERVER_ADR
 
 const $host = axios.create({
