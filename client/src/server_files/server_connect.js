6dd7e282345cbd26c2586070038a189c5720ad19
Noxlight7 <noxlight7@mail.ru>
2024-06-25 19:51:04 +0300

    change address for docker

--- client/src/server_files/server_connect.js
+++ client/src/server_files/server_connect.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const SERVER_ADR = '192.168.1.38:5000'
+export const SERVER_ADR = 'server:5000'
 export const SERVER = 'http://' + SERVER_ADR
 
 const $host = axios.create({
